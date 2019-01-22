8ff83d6a310376a31bd2c617b6a17a1f0be21370
jdbbaugh <jbaugh78@msn.com>
2019-01-22 12:00:43 -0600

    quicksave

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,11 @@
+export default class LocationList extends {
+  render() {
+    return(
+      <div>
+        <h3>Student Kennels</h3>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+      </div>
+    )
+  }
+}
