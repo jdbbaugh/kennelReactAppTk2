dd587ed7eb2ca59d60a800bdb76b6eaf7a9d3ad6
jdbbaugh <jbaugh78@msn.com>
2019-01-22 19:56:04 -0600

    first practice done

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,10 +1,19 @@
-export default class LocationList extends {
+import React, { Component } from 'react'
+
+export default class LocationList extends Component {
   render() {
     return(
       <div>
-        <h3>Student Kennels</h3>
-        <h4>Nashville North Location</h4>
-        <h5>500 Puppy Way</h5>
+        <section>
+          <h3>Student Kennels</h3>
+          <h4>Nashville North Location</h4>
+          <h5>500 Puppy Way</h5>
+        </section>
+        <section>
+          <h3>Graduate Kennels</h3>
+          <h4>Nashville South Location</h4>
+          <h5>2345 Dog St.</h5>
+        </section>
       </div>
     )
   }
