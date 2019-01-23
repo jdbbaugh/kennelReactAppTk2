57fe1c088925fb185c40125ff737eb3468432eb3
jdbbaugh <jbaugh78@msn.com>
2019-01-23 09:41:48 -0600

    quick save

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -55,16 +55,4 @@ export default class ApplicationViews extends Component {
             </React.Fragment>
         )
     }
-    deleteAnimal = id => {
-        return fetch(`http://localhost:5002/animals/${id}`, {
-            method: "DELETE"
-        })
-        .then(e => e.json())
-        .then(() => fetch(`http://localhost:5002/animals`))
-        .then(e => e.json())
-        .then(animals => this.setState({
-            animals: animals
-        })
-      )
-    }
 }
