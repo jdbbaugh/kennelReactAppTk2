a2779927573ffa838d6776454885df7ea8b186fd
jdbbaugh <jbaugh78@msn.com>
2019-01-24 09:20:55 -0600

    book examples functioning, now going into practices

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,7 +3,6 @@ import React, { Component } from "react"
 import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
-import SearchBar from './search/SearchBar'
 import OwnerList from './owner/OwnerList';
 
 
@@ -33,6 +32,19 @@ export default class ApplicationViews extends Component {
         .then(() => this.setState(newState))
 }
 
+deleteAnimal = id => {
+    return fetch(`http://localhost:5002/animals/${id}`, {
+        method: "DELETE"
+    })
+    .then(e => e.json())
+    .then(() => fetch(`http://localhost:5002/animals`))
+    .then(e => e.json())
+    .then(animals => this.setState({
+        animals: animals
+    })
+        )
+}
+
 
     render() {
         return (
@@ -41,16 +53,13 @@ export default class ApplicationViews extends Component {
                     return <LocationList locations={this.state.locations} />
                 }} />
                 <Route exact path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                return <AnimalList deleteAnimal={this.deleteAnimal} animals={this.state.animals} />
                 }} />
                 <Route exact path="/owners" render={(props) => {
                     return <OwnerList owners={this.state.owners} />
                 }} />
                 <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
-                }} />
-                <Route exact path="/search" render={(props) => {
-                    return <SearchBar />
                 }} />
             </React.Fragment>
         )
